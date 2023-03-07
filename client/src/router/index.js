f70243270764d826357db5c8ab95f1d79e15d57c
Igor Koneshov <igorkoneshov@mail.ru>
2023-03-07 15:24:56 +0400

    feat: add Register page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,47 +1,52 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView
-  },
-  {
-    path: "/about",
-    name: "about",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue")
-  },
-  {
-    path: "/markets",
-    name: "markets",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/MarketsView.vue")
-  },
-  {
-    path: "/convert",
-    name: "convert",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/ConvertView.vue")
-  },
-  {
-    path: "/profile",
-    name: "profile",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue")
-  },
-  {
-    path: "/admin",
-    name: "admin",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AdminView.vue")
-  },
+    {
+        path: "/",
+        name: "home",
+        component: HomeView
+    },
+    {
+        path: "/about",
+        name: "about",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/AboutView.vue")
+    },
+    {
+        path: "/markets",
+        name: "markets",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/MarketsView.vue")
+    },
+    {
+        path: "/convert",
+        name: "convert",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/ConvertView.vue")
+    },
+    {
+        path: "/profile",
+        name: "profile",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/AboutView.vue")
+    },
+    {
+        path: "/register",
+        name: "register",
+        component: () => import("../views/RegisterView.vue")
+    },
+    {
+        path: "/admin",
+        name: "admin",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/AdminView.vue")
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 });
 
 export default router;
---
+++ client/src/views/RegisterView.vue
@@ -0,0 +1,170 @@
+<template>
+    <div class="page conteiner">
+        <form class="form"><h2 class="container__title">Создание личного аккаунта</h2>
+            <div class="form__input-container">
+                <label class="lable">Адрес личной эл.почты</label>
+                <input v-model="login" class="input"/>
+            </div>
+            <div class="form__input-container">
+                <label class="lable">пароль</label>
+                <input v-model="password" type="password" class="input"/>
+            </div>
+            <div class="form__agree">
+                <input v-model="isAgree" type="checkbox"
+                       value="Я согласен получать маркетинговые материалы от Binance."/>
+                <p class="text">Я согласен получать маркетинговые материалы от My Binance.</p>
+            </div>
+            <button type="submit" class="button">Создание личного аккаунта</button>
+            <div class="need-account">
+                Уже есть аккаунт?
+                <router-link class="need-account__link" to="/login">Войти</router-link>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name: "RegisterView",
+    data() {
+        return {
+            login: '',
+            password: '',
+            isAgree: false,
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.conteiner {
+  max-width: 1280px;
+  width: 100%;
+  margin: 0 auto;
+  padding-top: 40px;
+
+  &__title {
+    margin: 0px 0px 16px;
+    min-width: 0px;
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 40px;
+    color: rgb(30, 35, 41);
+  }
+}
+
+.form {
+  max-width: 384px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 0 auto;
+  gap: 24px;
+
+  &__agree {
+    display: flex;
+    align-items: start;
+    gap: 10px;
+
+    p {
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 20px;
+    }
+  }
+
+  &__input-container {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+.lable {
+  margin: 0px 0px 4px;
+  min-width: 0px;
+  display: flex;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  width: 100%;
+  -moz-box-align: center;
+  align-items: center;
+  color: rgb(30, 35, 41);
+  cursor: auto;
+}
+
+.input {
+  color: rgb(30, 35, 41);
+  font-size: 14px;
+  padding-left: 12px;
+  padding-right: 12px;
+  outline: none;
+  background-color: inherit;
+  opacity: 1;
+  width: 100%;
+  line-height: 1.6;
+  height: 48px;
+  border: 1px solid rgb(234, 236, 239);
+  border-radius: 4px;
+
+  &:focus {
+    border-color: rgb(240, 185, 11);
+  }
+}
+
+.button {
+  background-color: rgb(252, 213, 53);
+  opacity: 0.9;
+  margin: 0px;
+  appearance: none;
+  user-select: none;
+  cursor: pointer;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: inline-flex;
+  -moz-box-align: center;
+  align-items: center;
+  -moz-box-pack: center;
+  justify-content: center;
+  box-sizing: border-box;
+  font-size: 16px;
+  font-family: inherit;
+  font-weight: 500;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  padding: 12px 24px;
+  line-height: 24px;
+  min-width: 80px;
+  word-break: keep-all;
+  color: rgb(24, 26, 32);
+  border-radius: 4px;
+  min-height: 24px;
+  border: medium none;
+  background-image: none;
+  white-space: normal;
+  width: 100%;
+  height: 48px;
+}
+
+.need-account {
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  color: rgb(112, 122, 138);
+
+  &__link {
+    font-size: 14px;
+    line-height: 20px;
+    color: rgb(201, 148, 0);
+    font-weight: 500;
+    cursor: pointer;
+    display: inline;
+    text-decoration: none;
+
+
+    &:hover {
+      color: rgb(252, 213, 53);
+      text-decoration: underline;
+    }
+  }
+}
+</style>
