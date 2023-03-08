7f4f75052c27598fca84f67390748c0a89db762a
Igor Koneshov <igorkoneshov@mail.ru>
2023-03-09 00:26:22 +0400

    feat: hidden route from not Auth user

--- client/src/components/Header/Header.vue
+++ client/src/components/Header/Header.vue
@@ -1,18 +1,57 @@
 <template>
   <header class="header">
-   <router-link to="/"><img alt="logo" class="header__logo" src="@/assets/logo.svg" /></router-link>
+    <router-link to="/"
+      ><img alt="logo" class="header__logo" src="@/assets/logo.svg"
+    /></router-link>
     <nav class="header__nav">
-      <router-link active-class="header__link_active" class="header__link" to="/">Главная</router-link>
-      <router-link active-class="header__link_active" class="header__link" to="/about">О нас</router-link>
-      <router-link active-class="header__link_active" class="header__link" to="/markets">Рынок</router-link>
-      <router-link active-class="header__link_active" class="header__link" to="/convert">Торговля</router-link>
-      <router-link active-class="header__link_active" class="header__link" to="/profile">Личный кабинет</router-link>
+      <router-link
+        active-class="header__link_active"
+        class="header__link"
+        to="/"
+        >Главная</router-link
+      >
+      <router-link
+        active-class="header__link_active"
+        class="header__link"
+        to="/about"
+        >О нас</router-link
+      >
+      <router-link
+        v-if="getAuthStatus"
+        active-class="header__link_active"
+        class="header__link"
+        to="/markets"
+        >Рынок</router-link
+      >
+      <router-link
+        v-if="getAuthStatus"
+        active-class="header__link_active"
+        class="header__link"
+        to="/convert"
+        >Торговля</router-link
+      >
+      <router-link
+        v-if="!getAuthStatus"
+        active-class="header__link_active"
+        class="header__link"
+        to="/login"
+        >Войти</router-link
+      >
+      <router-link
+        v-if="getAuthStatus"
+        active-class="header__link_active"
+        class="header__link"
+        to="/profile"
+        >Личный кабинет</router-link
+      >
     </nav>
   </header>
 </template>
 <script>
+import { mapGetters } from "vuex";
 export default {
-  name: "Header"
+  name: "Header",
+  computed: mapGetters(["getAuthStatus"]),
 };
 </script>
 
