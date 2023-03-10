272497ed8224f2132ec17c4b2551964d144b86fb
Igor Koneshov <igorkoneshov@mail.ru>
2023-03-10 11:35:00 +0400

    feat: add WebConfig

--- server/src/main/java/com/example/mybinance/config/WebConfig.java
+++ server/src/main/java/com/example/mybinance/config/WebConfig.java
@@ -1,2 +1,19 @@
-package com.example.mybinance.config;public class WebConfig {
+package com.example.mybinance.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
 }
+
